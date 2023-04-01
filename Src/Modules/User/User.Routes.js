73b675754551e0c6d4ff9249bf8bd6f43dde9cd3
ZeyadWael10 <85319582+ZeyadWael10@users.noreply.github.com>
2023-04-02 01:09:07 +0200

    Update User.Routes.js

--- Src/Modules/User/User.Routes.js
+++ Src/Modules/User/User.Routes.js
@@ -3,11 +3,13 @@ const router = Router();
 import * as UserController from "./User.Controller.js"
 import { asyncHandler } from "../../Utils/ErrorHandling.js";
 import { auth } from '../../Middlewares/Authentication.js';
-router.post('/signup',asyncHandler(UserController.signUp))
-router.post('/signin',asyncHandler(UserController.login))
-router.patch('/update',auth(),asyncHandler(UserController.updateUser))
+import { ValidationFunction } from '../../Middlewares/Validation.js';
+import { forgetPasswordValidation, loginValidation, resetPasswordValidation, signUpValidatinon, updateValidation } from './User.Validation.js';
+router.post('/signup',ValidationFunction(signUpValidatinon),asyncHandler(UserController.signUp))
+router.post('/signin',ValidationFunction(loginValidation),asyncHandler(UserController.login))
+router.patch('/update',ValidationFunction(updateValidation),auth(),asyncHandler(UserController.updateUser))
 router.post('/logout',auth(),asyncHandler(UserController.logout))
 router.delete('/delete',auth(),asyncHandler(UserController.deleteUser))
-router.post('/forget',asyncHandler(UserController.forgetPassword)) // request forget password (before login)
-router.post('/reset',asyncHandler(UserController.ResetPassword)) // actual function for reseting password (before login and after sending email)
+router.post('/forget',ValidationFunction(forgetPasswordValidation),asyncHandler(UserController.forgetPassword)) // request forget password (before login)
+router.post('/reset',ValidationFunction(resetPasswordValidation),asyncHandler(UserController.ResetPassword)) // actual function for reseting password (before login and after sending email)
 export default router;
